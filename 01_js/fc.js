de82a1fa8b264218bd720ab7574431d23f3fbbd2
Locke00 <hoseluisperez@hotmail.com>
2023-12-19 22:27:30 -0300

    ordenando (#5)

--- 01_js/fc.js
+++ 01_js/fc.js
@@ -32,3 +32,30 @@ console.log(cad1);   // muestra: texto de cadena de caracteres
 console.log(cad2);   // muestra: Linea1 de caracteres
                      //               Linea2 de caracteres
 console.log(cad3);   // muestra: texto de cadena de caracteres --expresion_adicional-- texto adicional
+
+//.map() : transforma cada elemento del array en otra cosa
+//esta funcion flecha va a tomar cada elemento del arreglo pasado como parametro y lo va a transformar en otro arreglo del mismo tamaño
+//solo q cada uno de sus elementos va a ser un objeto con dos propiedades: la propiedad valor q va a tener el valor q tenia ese elemento
+// y la propiedad tipoDeDato q va a devolver el tipo de dato del q se trata
+function corroborar(arreglo){
+  const tipoDeDato = arreglo.map(cadaUno =>({valor: cadaUno, tipoDeDato: typeof cadaUno  }))
+  console.log(tipoDeDato);
+}
+
+const resultado = corroborar([1,"hola",null,false])
+/* devuelve:
+[
+  { valor: 1, tipoDeDato: 'number' },
+  { valor: 'hola', tipoDeDato: 'string' },
+  { valor: null, tipoDeDato: 'object' },
+  { valor: false, tipoDeDato: 'boolean' }
+] */
+
+corroborar([NaN,undefined,true,imprimir,resultado])
+/*[
+  { valor: NaN, tipoDeDato: 'number' },
+  { valor: undefined, tipoDeDato: 'undefined' },
+  { valor: true, tipoDeDato: 'boolean' },
+  { valor: [Function: imprimir], tipoDeDato: 'function' },
+  { valor: undefined, tipoDeDato: 'undefined' }
+]*/
