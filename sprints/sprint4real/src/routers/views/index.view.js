4771466f06390155879330a40e59d092dba84adb
joseluisperez <hoseluisperez@hotmail.com>
2024-01-17 00:21:37 -0300

    sprint4real

--- sprints/sprint4real/src/routers/views/index.view.js
+++ sprints/sprint4real/src/routers/views/index.view.js
@@ -1,16 +1,21 @@
 import { Router } from "express";
 import productsManager from "../../data/fs/productFs.manager.js";
+
+import registerRouter from "./register.view.js";
+
 const viewsRouter = Router();
 
 viewsRouter.get("/", (req, res, next) => {
   //tengo q agregar el parametro next, para reenviarlo, para usar el errorHandler
   try {
-    const products = productsManager.read()
-    const date = new Date()
-    return res.render("index", {products,date}); //devuelvo la vista q tengo q renderizar
+    const products = productsManager.read();
+    const date = new Date();
+    return res.render("index", { products, date }); //devuelvo la vista q tengo q renderizar
     // el 1er parametro es la vista, dsp va un objeto vacio
-  } catch (error) {                      
+  } catch (error) {
     next(error); // en el catch tengo q poner next(error), para q sea manejado x el errorHandler
   }
 });
+viewsRouter.use("/register", registerRouter);
+
 export default viewsRouter;
