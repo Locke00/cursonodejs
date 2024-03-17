493e9c29953ac8b2511720fd447ab90837f7587f
joseluisperez <hoseluisperez@hotmail.com>
2024-03-17 18:42:41 -0300

    sprint7b

--- sprints/sprint7/src/routers/views/form.view.js
+++ sprints/sprint7/src/routers/views/form.view.js
@@ -4,7 +4,14 @@ const formRouter = Router();
 
 formRouter.get("/", (req, res, next) => {
   try {
-    return res.render("/form", { title: "Formulario" });
+    const user = res.locals.user
+    console.log(user);
+    if (!user) {
+      return res.redirect("/");
+    }
+
+
+    return res.render("form", { title: "Formulario" });
   } catch (error) {
     console.log("x aqui paso -catch");
     next(error);
--- sprints/sprint7/src/routers/views/order.view.js
+++ sprints/sprint7/src/routers/views/order.view.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { verifytoken } from "../../utils/token.util.js";
+
 
 import { orders, users } from "../../data/mongo/manager.mongo.js";
 
@@ -8,37 +10,33 @@ const orderRouter = Router();
 
 orderRouter.get("/", async (req, res, next) => {
   try {
-    
+    const user = res.locals.user
+    if (!user) {
+      return res.redirect("/");
+    }
     const options = {
       limit: req.query.limit || 10,
       page: req.query.page || 1,
       sort: { title: 1 },
       lean: true,
     };
-    console.log("intento-start");
-    //console.log(req.body);
-    //console.log(req.user.email);
-    
-    const token = req.cookies.token
-    console.log('token jwt: ', token);
-    
-    const user_id = '65f3b1e3d109c763063c7f60';
-    //const user = await users.readByEmail(req.user.email);
-    console.log("intento-end");
     const filter = {
       //user_id: user._id,
-      user_id: user_id,
+      user_id: user._id.toString(),
     };
     const allBSON = await orders.read({ filter, options });
     const allBSONstr = JSON.stringify(allBSON);
     const all = JSON.parse(allBSONstr);  //queda el objeto en formato json
 
     //console.log(all.docs[0].product_id);
-    console.log(all);
-    return res.render("orders", { title: "MY CART", orders: all.docs });
+    return res.render("orders", { 
+      title: "MY CiART",
+      orders: all.docs,
+    } ) ;
   } catch (error) {
+    console.log({error})
     return res.render("orders", {
-      title: "MY CART",
+      title: "MY wCART",
       message: "NO ORDERS YET!",
     });
   }
