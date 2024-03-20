ee8aefa444e8904c2b3c757cd2b3704f586fcb91
joseluisperez <hoseluisperez@hotmail.com>
2024-03-19 23:48:56 -0300

    sprint9

--- sprints/sprint9/src/routers/api/sessions.router.api.js
+++ sprints/sprint9/src/routers/api/sessions.router.api.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { users } from "../../data/mongo/manager.mongo.js";
+
 import has8char from "../../middlewares/has8char.mid.js";
-import isValidPassword from "../../middlewares/isValidPass.mid.js";
+import isValidPassword  from "../../middlewares/isValidPass.mid.js"
 import passport from "../../middlewares/passport.mid.js";
-import passCallBack from "../../middlewares/passCallBack.mid.js"
 
 const sessionsRouter = Router();
+
 
 //register
 //(<nombre_del_endpound>, (cheaquear q la contraseña tenga 8 caracteres), <middleware con la estrategia de registro>, <callback>  )
@@ -14,11 +15,10 @@ const sessionsRouter = Router();
 sessionsRouter.post(
   "/register",
   has8char,
-  /*passport.authenticate("register", {
+  passport.authenticate("register", {
     session: false,
     failureRedirect: "/api/sessions/badauth",
-  }),*/
-  passCallBack("register"),
+  }),
   async (req, res, next) => {
     //session: false  // permite q no sea necesario hacer un serializado. q el usuario no se transforme en un id, y dsp en un usuario, etc...
     try {
@@ -32,34 +32,38 @@ sessionsRouter.post(
   }
 );
 
-//login
+
+
+
+//login   //ej de usuario: franco3@gmail.com/hola1234
 //ya q passport se va a encargar de verificar el usuario y la contraseña
 //entonces login solo se va a encargar de manejar la respuesta:
 sessionsRouter.post(
   "/login",
-  /*passport.authenticate("login", {
+  passport.authenticate("login", {
     session: false,
     failureRedirect: "/api/sessions/badauth",
-  }),*/
-  passCallBack("login"),
+  }),
   async (req, res, next) => {
     try {
-      // httpOnly: true   permite q no se pueda acceder a la cookie dsd el navegador
       return res
-        .cookie("token", req.token, {
+        .cookie("token", req.token, {        // el valor del token esta en el objeto requerimientos
           maxAge: 7 * 24 * 60 * 60,
-          httpOnly: true,
-        })
+          httpOnly: true,  // httpOnly: true   permite q no se pueda acceder a la cookie dsd el navegador
+        })   // esto permite q cuando se loguee bien al usuario se le cree el cookie 
         .json({
           statusCode: 200,
           message: "Logged in",
-          //session: req.session, no voy a devolver la sesion
+          //token: req.token  // no voy a enviar token x aqui, sino x el cookie
         });
     } catch (error) {
       return next(error);
     }
   }
 );
+
+
+
 
 //me,   vamos a hacer un post para ver mis datos de sesion
 sessionsRouter.post("/", async (req, res, next) => {
@@ -80,37 +84,38 @@ sessionsRouter.post("/", async (req, res, next) => {
   }
 });
 
-//signout
-//failureRedirect: "/api/sessions/signout/cb"  le agrego para q en caso q falle(x ej si intento desloguearme estando deslogueado, no me aparezca el error de unauthorized)
-sessionsRouter.post(
-  "/signout",
-  /*passport.authenticate("jwt", {  //este bloque lo comento xq ya no lo usar, sino q en su lugar usaré passCallBack("jwt")
-    session: false,
-    failureRedirect: "/api/sessions/signout/cb",
-  }),*/
-  passCallBack("jwt"),  //en el parentesis el paso eltipo de estrategi
-  async (req, res, next) => {
-    try {
-      return res.clearCookie("token").json({
-        statusCode: 200,
-        message: "Signed out",
-      });
-    } catch (error) {
-      return next(error);
-    }
-  }
-);
 
-sessionsRouter.get("/signout/cb", (req, res, next) => {
+
+/*//signout
+sessionsRouter.post("/signout", async (req, res, next) => {
   try {
+    req.logout();
     return res.json({
-      statusCode: 400,
-      message: "Already done",
+      statusCode: 200,
+      message: "Signed out",
     });
   } catch (error) {
-    next(error);
+    return next(error);
+  }
+});*/
+
+sessionsRouter.post("/signout", async (req, res, next) => {
+  try {
+    return res.clearCookie("token").json({  //tengo q eliminar la cookie
+      statusCode: 200,
+      message: "Signed out",
+      //session: req.session, no voy a devolver la sesion
+    });
+  } catch (error) {
+    return next(error);
   }
 });
+
+
+
+
+
+
 
 sessionsRouter.get("/badauth", (req, res, next) => {
   try {
@@ -123,12 +128,13 @@ sessionsRouter.get("/badauth", (req, res, next) => {
   }
 });
 
+
 //google      , cuando vaya a este endpoint me va a llevar a la pantalla de consentimiento
 //si da exito, va a ir automaticamente al endpoint: /api/google/callback
 //como lo definimos en el parametro callbackURL, de la estrategia de google, q esta en passport.mid.js
 //lo voy a probar con get para probarlo con la url en el navegador
 //DEBE SER UN POST PARA LO Q EJECUTE EL EVENTO DE CLICK EN EL BOTON DE GOOGLE
-sessionsRouter.get(
+sessionsRouter.post(
   "/google",
   passport.authenticate("google", { scope: ["email", "profile"] }) // cuando pongo asi el scope le digo q tome como parametro principal
   //el email, y q ponga todos los datos en profile
@@ -154,5 +160,7 @@ sessionsRouter.get(
     }
   }
 );
+
+
 
 export default sessionsRouter;
