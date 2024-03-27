a9dec061d8e8ac84b57455a67bcaca49a27f9ce5
joseluisperez <hoseluisperez@hotmail.com>
2024-03-27 12:48:02 -0300

    26_layers

---
+++ 26_layers/src/services/events.service.js
@@ -0,0 +1,52 @@
+import { events } from "../data/mongo/manager.mongo.js";
+
+class EventsService {
+  constructor() {
+    this.model = events;
+  }
+  create = async (data) => {
+    try {
+      const response = await this.model.create(data);
+      return response;
+    } catch (error) {
+      //error.statusCode = xxx 
+      throw error;  //aqui no tengo para hacer next, asiq hago throw
+    }
+  };
+  read = async ({ filter, options }) => {
+    try {
+      console.log(this.model);
+      const response = await this.model.read({ filter, options });
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  };
+  readOne = async (id) => {
+    try {
+      const response = await this.model.readOne(id);
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  };
+  update = async (id, data) => {
+    try {
+      const response = await this.model.update(id, data);
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  };
+  destroy = async (id) => {
+    try {
+      const response = await this.model.destroy(id);
+      return response;
+    } catch (error) {
+      throw error;
+    }
+  };
+}
+
+const service = new EventsService();
+export default service;
